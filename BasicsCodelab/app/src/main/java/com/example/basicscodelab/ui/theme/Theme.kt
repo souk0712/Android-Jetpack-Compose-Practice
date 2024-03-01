1d82219b7f6936ef6720d10cabb3699189e560bc
이아름 <souk0712@naver.com>
2024-03-02 00:37:14 +0900

    feat: jetpack compose 기초 연습
    - 앱의 스타일 지정 및 테마 설정

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
@@ -10,21 +10,26 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
+// dynamicColor 적용으로 색상 변화가 없음
+// API 수준이 31(적응형 색이 도입된 Android S에 해당)보다 낮은 기기에서 앱을 실행
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
