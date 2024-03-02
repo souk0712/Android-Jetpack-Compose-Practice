8bafaf0f4a25588aeacbcbcb9856d4876637a644
이아름 <souk0712@naver.com>
2024-03-03 00:12:29 +0900

    feat: Compose의 기본 레이아웃
    MySoothe 앱 - 창 크기

--- BasicLayoutsCodelab/app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
@@ -43,6 +43,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -55,9 +59,13 @@ import androidx.compose.ui.unit.dp
 import com.example.basiclayoutscodelab.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MySootheApp() }
+        setContent {
+            val windowSizeClass = calculateWindowSizeClass(this)
+            MySootheApp(windowSizeClass)
+        }
     }
 }
 
@@ -319,9 +327,18 @@ fun MySootheAppLandscape() {
 }
 
 // Step: MySoothe App
+// 창 크기 클래스 너비에는 소형, 중형, 확장형 세 가지
 @Composable
-fun MySootheApp() {
-    // Implement composable here
+fun MySootheApp(windowSize: WindowSizeClass) {
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MySootheAppPortrait()
+        }
+
+        WindowWidthSizeClass.Expanded -> {
+            MySootheAppLandscape()
+        }
+    }
 }
 
 private val alignYourBodyData = listOf(
